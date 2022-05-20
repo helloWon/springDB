082b420297bbecd8022cad02d675aaf98086c7d9
helloWon <leeseongwon13@gmail.com>
2022-05-21 03:08:30 +0900

    [JDBC 이해] 조회

--- src/main/java/hello/jdbc/domain/Member.java
+++ src/main/java/hello/jdbc/domain/Member.java
@@ -2,7 +2,7 @@ package hello.jdbc.domain;
 
 import lombok.Data;
 
-@Data
+@Data // equals 와 hashcode Method 자동생성 해줌
 public class Member {
 
     private String memberId;
--- src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
+++ src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.NoSuchElementException;
 
 import hello.jdbc.connection.DBConnectionUtil;
 import hello.jdbc.domain.Member;
@@ -29,6 +30,34 @@ public class MemberRepositoryV0 {
             pstmt.setInt(2, member.getMoney());
             int count = pstmt.executeUpdate(); // 영향받은 row 개수
             return member;
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        } finally {
+            close(con, pstmt, null);
+        }
+    }
+
+    public Member findById(String memberId) throws SQLException {
+        String sql = "select * from member where member_id = ?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+            rs = pstmt.executeQuery();
+            if (rs.next()) {
+                Member member = new Member();
+                member.setMemberId(rs.getString("member_id"));
+                member.setMoney(rs.getInt("money"));
+                return member;
+            } else {
+                throw new NoSuchElementException("member not found memberId =" + memberId);
+            }
         } catch (SQLException e) {
             log.error("db error", e);
             throw e;
